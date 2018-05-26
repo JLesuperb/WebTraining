1fa3218b12ea8d089496f38f0f6bdc1ca11560b7
JonathanLesuperb <lesuperbjonathan@gmail.com>
2018-05-26 08:38:52 +0100

    Correction

---
+++ controllers/errorController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: JonathanLesuperb
+ * Date: 2018/05/22
+ * Time: 9:22 AM
+ */
+
+class ErrorController extends Controller
+{
+
+    public function index()
+    {
+        $this->render('error/index');
+    }
+}
